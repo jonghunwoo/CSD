81b7bdbb71126e6228f288600e7e51d60665a005
woojonghun <jonghun.digitalfactory@gmail.com>
2019-09-01 18:12:05 +0900

    add date conversion code

--- spool_procurement.py
+++ spool_procurement.py
@@ -7,17 +7,17 @@ import datetime
 import numpy as np
 
 # ./data 폴더에 해당 파일이 없으면 실행
-if not os.path.isfile('./data/spool_list.csv'):
+if not os.path.isfile('./data/spool_list_rev.csv'):
     #urlretrieve를 이용하여 파일에 직접 저장하는 코드
-    url = "https://raw.githubusercontent.com/jonghunwoo/public/master/spool_list.xlsx"
-    filename = "./data/spool_list.xlsx"
+    url = "https://raw.githubusercontent.com/jonghunwoo/public/master/spool_list_rev.xlsx"
+    filename = "./data/spool_list_rev.xlsx"
     urllib.request.urlretrieve(url, filename)
 
     # Excel 파일을 csv로 변환하는 함수 정의
     def csv_from_excel():
-        workbook = xlrd.open_workbook('./data/spool_list.xlsx')
-        worksheet = workbook.sheet_by_name('record')
-        csv_file = open('./data/spool_list.csv', 'w')
+        workbook = xlrd.open_workbook('./data/spool_list_rev.xlsx')
+        worksheet = workbook.sheet_by_name('spool_list_rev')
+        csv_file = open('./data/spool_list_rev.csv', 'w')
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
         for row_num in range(worksheet.nrows):
@@ -28,4 +28,16 @@ if not os.path.isfile('./data/spool_list.csv'):
     csv_from_excel()
 
 # csv 파일 pandas 객체 생성
-data = pd.read_csv('./data/spool_list.csv')
+data = pd.read_csv('./data/spool_list_rev.csv')
+
+# 필요한 column 로딩 - df에 저장
+df = data[["block", "spool_no", "line_no", "dia", "dia_unit", "length", "weight", "wo", "material_out", "cutting", "fitup", "welding", "inspection","nde","palleting", "spool_out","coating", "painting_in", "painting_start", "painting", "stock_in", "stock_out", "on_deck", "in_position", "install_fitup", "installation"]]
+#print(df.head(40))
+
+for date in ["wo", "material_out", "cutting", "fitup", "welding", "inspection","nde","palleting", "spool_out","coating", "painting", "painting_in", "painting_start", "painting", "stock_in", "stock_out", "on_deck", "in_position", "install_fitup", "installation"]:
+    df[date] = pd.TimedeltaIndex(df[date], unit='d') + datetime.datetime(1899,12,30)
+
+#print(df[["wo", "material_out", "cutting", "fitup", "welding", "inspection","nde","palleting", "spool_out","coating", "painting_in", "painting_start", "painting", "stock_in", "stock_out", "on_deck", "in_position", "install_fitup", "installation"]].head(10))
+print(df[["spool_no", "wo", "material_out", "cutting"]].head(10))
+
+#print(df.head(40))
